3b592428bf72e6b6a5653e7d8a9353122965ba1f
Tox-word <gg.toxic.pro123@gmail.com>
2026-03-04 23:39:25 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Токен от @BotFather
+BOT_TOKEN = "8204017321:AAGepYmuggdM_WHodywL8_DaKa2k4oGUaf8"
+
+# Твой цифровой ID (можно узнать в боте @userinfobot)
+ADMIN_ID = 5500020741  
+
+# Ссылка на твою страницу с Adsgram (когда создашь index.html)
+ADSGRAM_URL = "https://tox-word.github.io/my-ads/"
+
+# Настройки экономики
+MIN_WITHDRAW = 200        # Минималка для вывода (в звездах)
+AD_REWARD = 0.5           # Награда за 1 просмотр видео
+REF_BONUS = 0.2           # % от заработка реферала (0.2 = 20%)
+
+# Курс (для информации пользователю)
+EXCHANGE_RATE = "100 ⭐ = 1.5$"
+
+REQUIRED_CHANNELS = ["@твой_канал_1", "@твой_канал_2"] # Юзернеймы каналов
